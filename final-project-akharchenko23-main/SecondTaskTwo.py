701aef5290c10874a86953e0a28cf9f3d7a46e03
akharchenko23 <akharchenko@kse.org.ua>
2024-04-02 00:27:59 +0300

    таке враження, що щось не так з однією з діаграм. вони мають різні рзподіли наприкінці

--- final-project-akharchenko23-main/SecondTaskTwo.py
+++ final-project-akharchenko23-main/SecondTaskTwo.py
@@ -3,15 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 netflix_df = pd.read_csv("netflix_dataset.csv")
+
+
 # Group the dataset by release year and count the number of content releases for each year
-content_by_year = netflix_df.groupby('release_year').size().reset_index(name='count')
+content_by_year = netflix_df['release_year'].value_counts().sort_index()
 
 # Plot the trend of content production over the years
 plt.figure(figsize=(12, 6))
-sns.lineplot(x='release_year', y='count', data=content_by_year, marker='o', color='red')
+sns.lineplot(x=content_by_year.index, y=content_by_year.values, marker='o', color='skyblue')
 plt.title('Trend of Content Production Over the Years')
 plt.xlabel('Release Year')
 plt.ylabel('Count of New Content')
 plt.xticks(rotation=45)
 plt.grid(True)
 plt.show()
+
